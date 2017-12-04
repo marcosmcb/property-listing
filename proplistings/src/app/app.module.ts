702afaed694c15fa1d6e0730b490ee835569412b
Marcos Cavalcante <marcos.bcc2011@gmail.com>
2017-12-04 22:06:48 +0000

    Add bug on service file for firebase

--- proplistings/src/app/app.module.ts
+++ proplistings/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule, Routes } from '@angular/router';
+import { AngularFireModule } from 'angularfire2';
+import { FirebaseService } from './services/firebase.service';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
@@ -12,6 +14,13 @@ import { ListingComponent } from './components/listing/listing.component';
 import { AddListingComponent } from './components/add-listing/add-listing.component';
 import { EditListingComponent } from './components/edit-listing/edit-listing.component';
 
+export const firebaseConfig = {
+    apiKey: 'AIzaSyBhiY-e9lYZ0M1E_q9P_w-xKuKto7rTFmY',
+    authDomain: 'property-listing-ef822.firebaseapp.com',
+    databaseUrl: 'https://property-listing-ef822.firebaseio.com',
+    storageBucket: 'property-listing-ef822.appspot.com',
+    messagingSenderId: '696106016535'
+};
 
 const appRoutes : Routes = [
     {path: '', component: HomeComponent},
@@ -33,9 +42,10 @@ const appRoutes : Routes = [
     BrowserModule,
     FormsModule,
     HttpModule,
+    AngularFireModule.initializeApp( firebaseConfig ),
     RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [FirebaseService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
